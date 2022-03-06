71df0b8edb838ab7c371e498068786c3f3dc1a86
Jasmine Santa Maria <89921853+JASMINETUFF@users.noreply.github.com>
2022-03-06 06:00:18 -0800

    updates

--- client/src/components/Form/picks.js
+++ client/src/components/Form/picks.js
@@ -1,23 +1,23 @@
 
 
-import React from "react";
-import Modal from "../Modal/index";
+// import React from "react";
+// import Modal from "./page2";
 
 
 
-const NoMatch = () => {
-  return (
-    <div>
-      <Modal>
-        <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
-      </Modal>
-    </div>
-  );
-};
+// const NoMatch = () => {
+//   return (
+//     <div>
+//       <Modal>
+//         <h1>404 Page Not Found</h1>
+//         <h1>
+//           <span role="img" aria-label="Face With Rolling Eyes Emoji">
+//             🙄
+//           </span>
+//         </h1>
+//       </Modal>
+//     </div>
+//   );
+// };
 
-export default NoMatch;
+// export default NoMatch;
--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,43 +1,43 @@
-import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Modal';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+// import React, { useEffect } from 'react';
+// import { useMutation } from '@apollo/client';
+// import Jumbotron from '../components/Modal';
+// import { ADD_ORDER } from '../utils/mutations';
+// import { idbPromise } from '../utils/helpers';
 
-function Success() {
-  const [addOrder] = useMutation(ADD_ORDER);
+// function Success() {
+//   const [addOrder] = useMutation(ADD_ORDER);
 
-  useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
+//   useEffect(() => {
+//     async function saveOrder() {
+//       const cart = await idbPromise('cart', 'get');
+//       const products = cart.map((item) => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
+//       if (products.length) {
+//         const { data } = await addOrder({ variables: { products } });
+//         const productData = data.addOrder.products;
 
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
+//         productData.forEach((item) => {
+//           idbPromise('cart', 'delete', item);
+//         });
+//       }
 
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
-    }
+//       setTimeout(() => {
+//         window.location.assign('/');
+//       }, 3000);
+//     }
 
-    saveOrder();
-  }, [addOrder]);
+//     saveOrder();
+//   }, [addOrder]);
 
-  return (
-    <div>
-      <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
-      </Jumbotron>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Jumbotron>
+//         <h1>Success!</h1>
+//         <h2>Thank you for your purchase!</h2>
+//         <h2>You will now be redirected to the home page</h2>
+//       </Jumbotron>
+//     </div>
+//   );
+// }
 
-export default Success;
+// export default Success;
