daa3b3e74d2cc1012031348a6d4ea31988be4698
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-03-21 20:08:29 -0700

    pushing up changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,7 +19,7 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 // Serve up static assets
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
